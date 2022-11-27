8d6dce475f30a7b8e9ce0b8a090da472a99709d2
Everton <everton.silva.s@hotmail.com>
2022-11-26 22:53:06 -0300

    Voltando ao arquivo original

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o Sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-// Mais um console de teste
-console.log("Olá Mundo")
-console.log("Enviando atualização para o GIT")

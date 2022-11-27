29169d0b535bb3c333335cf5107614ac4f3b8f79
Everton <everton.silva.s@hotmail.com>
2022-11-26 22:40:38 -0300

    adcionando um console

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o Sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // Mais um console de teste
-console.log("Olá Mundo")!
+console.log("Olá Mundo")
+console.log("Enviando atualização para o GIT")
